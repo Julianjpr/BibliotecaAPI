44d7718724acc562416beb4b902fc3ac81553123
Julianjpr <julian051698@gmail.com>
2026-06-29 11:37:39 -0400

    Add files via upload

---
+++ Libro.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaAPI.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Titulo { get; set; } = null!;
+
+        [Required]
+        public string Autor { get; set; } = null!;
+
+        public int AnioPublicacion { get; set; }
+
+        public string Genero { get; set; } = null!;
+
+        public int NumeroPaginas { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public bool Disponible { get; set; }
+    }
+}
