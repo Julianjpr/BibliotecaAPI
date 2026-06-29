f9966f81e4c22f515bb02b62f2e6ea80a7207a3b
Julianjpr <julian051698@gmail.com>
2026-06-29 11:35:30 -0400

    Add files via upload

---
+++ BibliotecaContext.cs
@@ -0,0 +1,15 @@
+﻿using BibliotecaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Data
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
